d2ee79b14175a9c74d1cd4bda7f6ab17e685b299
Gabriel Giuseppe do Nascimento <gabriel.giuseppe@live.com>
2022-07-07 17:12:45 -0300

    Start Configuration

---
+++ src/test/java/com/questgames/monster/tactics/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questgames.monster.tactics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
